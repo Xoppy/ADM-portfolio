ec7045497aed8f3623639ec5a5f52889edeb51bf
Angelo Di Marco <xopsdesign99@gmail.com>
2021-08-24 19:35:56 +0200

    Removed HTML/CSS mobile-nav switch and added javascript solution

--- docs/app.js
+++ docs/app.js
@@ -1,37 +1,27 @@
-/* Not working yet
-function smoothScroll(target, duration) {
-    var target = document.querySelector(target);
-    var targetPosition = target.getBoudningClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
+// Mobile nav bar menu toogle
 
-    function animation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
+let isOpen = false;
+let toogledMenuCss = 'transform: translateX(-100vw); z-index: 10; translate: ease 1ms;';
+let menuButton = document.querySelector("#mobile-nav");
+let mobileNav = document.querySelector(".mobile-nav");
 
-        var run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, run);
-
-        if (timeElapsed < duration) requestAnimationFrame(animation);
+function isOpenSwitch() {
+    if (isOpen === false) {
+        isOpen = true;
+    } else if (isOpen === true) {
+        isOpen = false;
     }
 
-    function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
+    if (isOpen === true) {
+        mobileNav.style.transform = "translateX(-100vw)";
+    } else {
+        mobileNav.style.transform = "translateX(100vw)"
     }
 
-    requestAnimationFrame(animation);
+    function menuClose() {
+        isOpen = false;
+        console.log(isOpen);
+    }
+
+    console.log(isOpen);
 }
-
-var about = document.querySelector(".about-btn");
-var pricing = document.querySelector(".pricing-btn");
-var portfolio = document.querySelector(".portfolio-btn");
-var contact = document.querySelector(".contact-selection-btn")
-
-pricing.addEventListener("click", function() {
-    smoothScroll(".pricings", 1000);
-});
-*/
